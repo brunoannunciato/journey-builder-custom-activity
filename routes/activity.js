725dd069817f0b5204b31193d0f61440765b7904
brunoannunciato <brunobannunciato@gmail.com>
2020-01-17 15:25:21 -0300

    create new branch

--- routes/activity.js
+++ routes/activity.js
@@ -1,9 +1,11 @@
 'use strict';
+var util = require('util');
 
+// Deps
 const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
-const util = require('util');
-const axios = require('axios');
+var util = require('util');
+var http = require('https');
 
 exports.logExecuteData = [];
 
@@ -28,7 +30,7 @@ function logData(req) {
         originalUrl: req.originalUrl
     });
     console.log("body: " + util.inspect(req.body));
-    console.log("headers: " + util.inspect(req.headers));
+    console.log("headers: " + req.headers);
     console.log("trailers: " + req.trailers);
     console.log("method: " + req.method);
     console.log("url: " + req.url);
@@ -46,74 +48,45 @@ function logData(req) {
     console.log("originalUrl: " + req.originalUrl);
 }
 
+/*
+ * POST Handler for / route of Activity (this is the edit route).
+ */
 exports.edit = function (req, res) {
+    // Data from the req and put it in an array accessible to the main app.
+    //console.log( req.body );
     logData(req);
     res.send(200, 'Edit');
 };
 
+/*
+ * POST Handler for /save/ route of Activity.
+ */
 exports.save = function (req, res) {
+    // Data from the req and put it in an array accessible to the main app.
+    //console.log( req.body );
     logData(req);
     res.send(200, 'Save');
 };
 
+/*
+ * POST Handler for /execute/ route of Activity.
+ */
 exports.execute = function (req, res) {
+
+    // example on how to decode JWT
     JWT(req.body, process.env.jwtSecret, (err, decoded) => {
+
+        // verification error -> unauthorized request
         if (err) {
             console.error(err);
             return res.status(401).end();
         }
 
-        console.log('buffer hex', req.body.toString('hex'));
-
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
+            
+            // decoded in arguments
             var decodedArgs = decoded.inArguments[0];
-            console.log('inArguments', JSON.stringify(decoded.inArguments));
-            console.log('decodedArgs', JSON.stringify(decodedArgs));
-
-            const headers = {
-                'Content-Type': 'application/json',
-                'Authorization': `Key ${process.env.BLIPAUTHORIZATIONKEY}`
-            }
-
-            const phoneNumber = decodedArgs['phoneNumber'];
-            const templateName = decodedArgs['templateName'];
-
-            const guid_id = uuidv4();
-
-            const post_save = {
-                "id": guid_id,
-                "to": "postmaster@wa.gw.msging.net",
-                "method": "get",
-                "uri": `lime://wa.gw.msging.net/accounts/+${phoneNumber}`
-            }
-
-            axios.post('https://msging.net/commands', post_save, { headers: headers }).then((res) => {
-                const post_hsm = {
-                    "id": guid_id,
-                    "to": `${phoneNumber}@wa.gw.msging.net`,
-                    "type": "application/json",
-                    "content": {
-                        "type": "hsm",
-                        "hsm": {
-                            "namespace": "0cf88f37_b88f_d3bd_b5be_f22588aabf89",
-                            "element_name": templateName,
-                            "fallback_lg": "pt",
-                            "fallback_lc": "BR",
-                            "localizable_params": []
-                        }
-                    }
-                }
-
-                axios.post('https://msging.net/messages', post_hsm, { headers: headers }).then((res) => {
-                    console.log(`Success send whatsapp to ${phoneNumber}`);
-                }).catch((err) => {
-                    console.log(`ERROR send whatsapp to ${phoneNumber}: ${err}`)
-                })
-            }).catch((err) => {
-                console.log(`ERROR verify whatsapp to ${phoneNumber}: ${err}`)
-            })
-
-
+            
             logData(req);
             res.send(200, 'Execute');
         } else {
@@ -123,19 +96,23 @@ exports.execute = function (req, res) {
     });
 };
 
+
+/*
+ * POST Handler for /publish/ route of Activity.
+ */
 exports.publish = function (req, res) {
+    // Data from the req and put it in an array accessible to the main app.
+    //console.log( req.body );
     logData(req);
     res.send(200, 'Publish');
 };
 
+/*
+ * POST Handler for /validate/ route of Activity.
+ */
 exports.validate = function (req, res) {
+    // Data from the req and put it in an array accessible to the main app.
+    //console.log( req.body );
     logData(req);
     res.send(200, 'Validate');
 };
-
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
