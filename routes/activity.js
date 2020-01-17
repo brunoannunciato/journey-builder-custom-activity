164a3359a1b639c30052e360498bc2fb2c4bc88f
brunoannunciato <brunobannunciato@gmail.com>
2020-01-17 13:34:01 -0300

    convert req.body to string

--- routes/activity.js
+++ routes/activity.js
@@ -168,7 +168,7 @@ exports.execute = function (req, res) {
  */
 exports.publish = function (req, res) {
 	// Data from the req and put it in an array accessible to the main app.
-	console.log( { reqBody: req.body } );
+	console.log( { reqBody: req.body.toString('hex') } );
 	logData(req);
 	res.send(200, 'Publish');
 
